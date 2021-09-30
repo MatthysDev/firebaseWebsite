9c4a5a20216d1f7dad4d02db011519c430641cf5
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-09-30 17:38:41 +0200

    add some image and css

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,16 +1,28 @@
 import * as React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../../components/layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPage = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>
-            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
-          </h2>
-          <p>Posted: {node.frontmatter.date}</p>
+          <p className="justify-center w-2/4 m-auto text-2xl text-left">
+            <Link className="pl-2 pt-4" to={`/blog/${node.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+            <GatsbyImage
+              image={image}
+              alt={data.mdx.frontmatter.hero_image_alt}
+              height="200px"
+              className="pt-4"
+            />
+            <p className="text-right text-base">
+              Posted: {node.frontmatter.date}
+            </p>
+          </p>
         </article>
       ))}
     </Layout>
@@ -18,7 +30,7 @@ const BlogPage = ({ data }) => {
 };
 
 export const query = graphql`
-  query {
+  query ($slug: String) {
     allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
         frontmatter {
@@ -27,6 +39,21 @@ export const query = graphql`
         }
         id
         slug
+      }
+    }
+    mdx(slug: { eq: $slug }) {
+      body
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
