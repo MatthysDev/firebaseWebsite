18ff76dbfca5cb6b733d10c2c04486a20a48df33
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-09-30 16:43:17 +0200

    add some css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,9 @@
 import * as React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle,
-} from "./layout.module.css";
+
+const activeStyles = {
+  color: "gray",
+};
 
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
@@ -20,34 +16,42 @@ const Layout = ({ pageTitle, children }) => {
     }
   `);
   return (
-    <div className={container}>
-      <title>
-        {pageTitle} | {data.site.siteMetadata.title}
-      </title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
+    <div className="bg-black h-screen text-white">
+      <div className="text-center m-auto font-sans w-3/4 pt-8">
+        <title>
+          {pageTitle} | {data.site.siteMetadata.title}
+        </title>
+        <header className="text-gray-600 text-5xl font-bold">
+          {data.site.siteMetadata.title}
+        </header>
+        <nav className="flex justify-center text-white pt-8 pb-4 text-xl">
+          <ul className="pl-0 flex list-none ">
+            <li className="pr-2 ">
+              <Link
+                to="/"
+                className="active:text-red"
+                activeStyle={activeStyles}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="pr-2">
+              <Link to="/about" activeStyle={activeStyles}>
+                About
+              </Link>
+            </li>
+            <li className="pr-2">
+              <Link to="/blog" activeStyle={activeStyles}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <h1 className="text-gray-500">{pageTitle}</h1>
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
