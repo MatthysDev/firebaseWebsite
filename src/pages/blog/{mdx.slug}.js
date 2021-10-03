8780de436b18b8eeb7811b699bf6ac3e33c6e09b
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-10-03 12:17:56 +0200

    css and first post

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -7,17 +7,29 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 const BlogPost = ({ data }) => {
   const image = getImage(data.mdx.frontmatter.hero_image);
   return (
-    <div className="bg-black">
+    <div className="bg-black overflow-auto h-screen">
       <Layout pageTitle={data.mdx.frontmatter.title}>
-        <p>{data.mdx.frontmatter.date}</p>
-        <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
-        <p>
-          Photo Credit:{" "}
-          <a href={data.mdx.frontmatter.hero_image_credit_link}>
-            {data.mdx.frontmatter.hero_image_credit_text}
-          </a>
-        </p>
-        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+        <div className="pb-12">
+          <p className=" pt-2 pb-2">{data.mdx.frontmatter.date}</p>
+          <div className="flex-initial">
+            <GatsbyImage
+              className="pt-6"
+              image={image}
+              alt={data.mdx.frontmatter.hero_image_alt}
+            />
+            <div>
+              <p className="pt-2 pb-2">
+                Photo Credit:{" "}
+                <a href={data.mdx.frontmatter.hero_image_credit_link}>
+                  {data.mdx.frontmatter.hero_image_credit_text}
+                </a>
+              </p>
+              <div className="text-left">
+                <MDXRenderer>{data.mdx.body}</MDXRenderer>
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     </div>
   );
