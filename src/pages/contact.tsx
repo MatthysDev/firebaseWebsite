2b9c9478a9d69f0c10cbb4309263a2db8bee0b2a
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-12-29 22:29:57 +0100

    isLoading iframe

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,17 +1,11 @@
 import * as React from "react";
 import Layout from "../components/layout";
-
-import Gravatar from "react-gravatar";
+import Airtable from "../components/Airtable";
 
 const Contact = () => {
   return (
     <Layout pageTitle="Contact">
-      <iframe
-        className="w-7/8 airtable-embed bg-transparent lg:w-2/3 m-auto mt-12 border-4 rounded-lg  border-purple-400"
-        src="https://airtable.com/embed/shrZLcDgGq0Kb6soN?backgroundColor=purple"
-        height="1200"
-        //style="background: transparent; border: 1px solid #ccc;"
-      ></iframe>
+      <Airtable />
     </Layout>
   );
 };
